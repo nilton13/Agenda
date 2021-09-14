4a0ea5a939465f6b79df4d3eb77e60d96700c595
nilton13 <jnvlfilho@hotmail.com>
2021-09-13 23:55:57 -0300

    Adicionando funções a área Administrativa

--- contatos/admin.py
+++ contatos/admin.py
@@ -1,2 +1,12 @@
 from django.contrib import admin
+from .models import Categoria, Contato
+
+class ContatoAdmin(admin.ModelAdmin):
+    list_display = ('id','nome','sobrenome', 'email', 'categoria')
+    list_display_links = ('id','nome','sobrenome')
+
+
+
+admin.site.register(Contato, ContatoAdmin)
+admin.site.register(Categoria)
 
--- contatos/models.py
+++ contatos/models.py
@@ -3,7 +3,12 @@ from django.utils import timezone
 
 
 class Categoria(models.Model):
-    nome: models.CharField(max_length=255)
+    nome = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.nome
+
+
 
 
 class Contato(models.Model):
@@ -14,6 +19,10 @@ class Contato(models.Model):
     data_criacao = models.DateTimeField(default=timezone.now)
     descricao = models.TextField(blank=True)
     categoria = models.ForeignKey(Categoria, on_delete=models.DO_NOTHING)
+
+    def __str__(self):
+        return self.nome
+
 
 
 
