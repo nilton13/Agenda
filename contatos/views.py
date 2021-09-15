3f105102ada2064f03ac7b78c28f06f9765aa9b1
nilton13 <jnvlfilho@hotmail.com>
2021-09-15 20:44:06 -0300

    Corrigindo aplicação e adicionando Filtros..

--- contatos/views.py
+++ contatos/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Contato
 from django.core.paginator import Paginator
+from django.http import Http404
 
 def index(request):
-    contatos = Contato.objects.all()
+    contatos = Contato.objects.order_by('nome').filter(
+
+    ) # Ordem decrescente('-nome')
     paginator = Paginator(contatos, 3)
 
     page = request.GET.get('p')
@@ -17,6 +20,10 @@ def index(request):
 def ver_contato(request, contato_id):
     # contato = Contato.objects.get(id=contato_id)
     contato = get_object_or_404(Contato,id=contato_id)
+
+    if not contato.mostrar:
+        raise Http404();
+
     return render(request, 'contatos/ver_contato.html',{
         'contato': contato
     })
