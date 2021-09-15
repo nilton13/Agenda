fe3fe0e7903554b0bb757c3e8047e899be09a9f0
nilton13 <jnvlfilho@hotmail.com>
2021-09-14 23:43:57 -0300

    Adicionando condicionais para mostrar arquivos no Front-End

--- contatos/admin.py
+++ contatos/admin.py
@@ -2,8 +2,9 @@ from django.contrib import admin
 from .models import Categoria, Contato
 
 class ContatoAdmin(admin.ModelAdmin):
-    list_display = ('id','nome','sobrenome', 'email', 'categoria')
+    list_display = ('id','nome','sobrenome','telefone', 'email', 'categoria', 'mostrar')
     list_display_links = ('id','nome','sobrenome')
+    list_editable = ('telefone', 'mostrar')
 
 
 
